c954d6408a55750a228d7a9058c356b357b4914d
hyc <1404733494@qq.com>
2022-06-10 18:52:45 +0800

    帖子详情

--- src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +29,8 @@ public class DiscussPostController {
     private HostHolder hostHolder;
     @Autowired
     private DiscussPostService discussPostService;
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -43,5 +48,14 @@ public class DiscussPostController {
 
         // 报错的情况，将来统一处理
         return CommunityUtil.getJSONString(0,"发布成功！");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}",method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int id, Model model){
+        DiscussPost discussPost = discussPostService.findDiscussPostById(id);
+        User user = userService.findUserById(discussPost.getUserId());
+        model.addAttribute("discussPost",discussPost);
+        model.addAttribute("user",user);
+        return "/site/discuss-detail";
     }
 }
