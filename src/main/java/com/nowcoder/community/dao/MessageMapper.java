fdd5d2e8dcf4d868f958759062aaf6c1a3fec9b8
hyc <1404733494@qq.com>
2022-06-22 11:08:23 +0800

    私信列表

---
+++ src/main/java/com/nowcoder/community/dao/MessageMapper.java
@@ -0,0 +1,31 @@
+package com.nowcoder.community.dao;
+
+
+import com.nowcoder.community.entity.Message;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author hyc
+ * @create 2022-06-21 21:48
+ */
+@Mapper
+public interface MessageMapper {
+
+    //用户的会话列表，针对每个会话只返回最新的一条私信
+    List<Message> selectConversations(int userId, int offset, int limit);
+
+    //用户的会话数量
+    int selectConversationCount(int userId);
+
+    //某个会话包含的私信列表
+    List<Message> selectLetters(String conversationId, int offset, int limit);
+
+    //某个会话包含的私信数量
+    int selectLetterCount(String conversationId);
+
+    //用户的未读私信数量；当conversationId不为空时，表示用户的某个会话的未读私信数量
+    int selectLetterUnreadCount(int userId, String conversationId);
+
+}
